50fa289e168243382e38738863ec21b925103e8d
徐永霖 <xuyonglin@intv.com.cn>
2017-10-24 23:21:02 +0800

    用户注册

--- application/user/controller/Ucenter.php
+++ application/user/controller/Ucenter.php
@@ -2,7 +2,9 @@
 namespace app\user\controller;
 
 use app\common\controller\BaseController;
+use app\user\model\UserInfo;
 use think\Request;
+use think\Session;
 
 class Ucenter extends BaseController
 {
@@ -16,4 +18,48 @@ class Ucenter extends BaseController
         $this->checkSecondPwd();
         return $this->fetch();
     }
+
+
+    public function reg_user(){
+        $this->checkSecondPwd();
+        return $this->fetch();
+    }
+
+    public function userlist(){
+        $this->checkSecondPwd();
+        $uid = Session::get('uid');
+        $model = new UserInfo();
+        $userList = $model->getMyUserList($uid);
+        var_dump($userList);exit;
+    }
+
+    public function useradd(){
+        $uinfo = [];
+        $uinfo['user_name'] = input('userid');
+        $uinfo['real_name'] = input('truename');
+        $parentName = input('tuname');
+        $uinfo['first_pw'] = input('rgpwd1');
+        $uinfo['second_pw'] = input('rgpwd2');
+        $uinfo['addr'] = input('rgaddress');
+        $uinfo['bank_name'] = input('bankname');
+        $uinfo['bank_no'] = input('bankcard');
+        $uinfo['bank_addr'] = input('bankaddress');
+        $model = new UserInfo();
+
+        $haveReg = $model->getHaveUserByUserName($uinfo['user_name']);
+        if($haveReg){
+            return $this->error('已存在的用户名');
+        }
+
+        $uinfo['parent_id'] = $model->getUserIdByUname($parentName);
+        $newUserId = $model->addUser($uinfo);
+        //if(0){
+        if($newUserId){
+            return $this->success('注册成功', '/user/ucenter/userlist');
+        }else{
+            return $this->error('注册失败');
+        }
+
+    }
+
 }
--- application/user/model/UserInfo.php
+++ application/user/model/UserInfo.php
@@ -10,5 +10,60 @@ class UserInfo extends Model{
         return $re;
     }
 
+    public function getHaveUserByUserName($uname){
+        return $this->where(['user_name' => $uname])->count();
+    }
+
+    public function getUserIdByUname($uname){
+        $re = $this->where(['user_name' => $uname])->find();
+        if(!$re){
+            return 0;
+        }
+        $uinfo = $re->toArray();
+        return $uinfo['id'];
+    }
+
+    public function getMyUserList($uid){
+        $level1 = $this->where(['parent_id' => $uid])->select();
+        $list1 = [];
+        foreach($level1 as $v){
+            $arr = $v->toArray();
+            $arr['level'] = 1;
+            $list1[] = $arr;
+        }
+        $where = [
+            'parent_id' => ['in', array_column($list1, 'id')],
+        ];
+        $level2 = $this->where($where)->select();
+        $list2 = [];
+        foreach($level2 as $v){
+            $arr = $v->toArray();
+            $arr['level'] = 2;
+            $list2[] = $arr;
+        }
+
+        $where = [
+            'parent_id' => ['in', array_column($list2, 'id')],
+        ];
+        $level3 = $this->where($where)->select();
+        $list3 = [];
+        foreach($level3 as $v){
+            $arr = $v->toArray();
+            $arr['level'] = 3;
+            $list3[] = $arr;
+        }
+
+        return ['level1' => $list1, 'level2' => $list2, 'level3' => $list3];
+
+
+    }
+
+    public function addUser($uinfo){
+        $uinfo['create_time'] = time();
+        $uinfo['update_time'] = time();
+        $re = $this->insert($uinfo);
+        return $re;
+    }
+
 }
 ?>
