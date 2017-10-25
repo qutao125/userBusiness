9aae24373329bc27db0d174e58e31c6a045c9267
bxwme <bxw534628@gmail.com>
2017-10-25 22:05:58 +0800

    添加类

--- application/user/controller/Sales.php
+++ application/user/controller/Sales.php
@@ -3,16 +3,27 @@ namespace app\user\controller;
 
 use app\common\controller\BaseController;
 use app\user\model\UserInfo;
+use app\user\model\UserAccountBanlance;
+use app\user\model\UserAccount;
 use think\Controller;
 use think\Session;
 
 class Sales extends BaseController
 {
     protected $nowController = 'sales';
+    private $uid = '';
+    public function __construct(){
+        $this->checkSecondPwd();
+        $this->uid = Session::get('uid');
+        parent::__construct();
+    }
     /**
      * 存券
     */
     public function saveList(){
+        $banlance = new UserAccountBanlance();
+        $list = $banlance->getListByUid($this->uid);
+        $this->assign('list',$list);
         return $this->fetch();
     }
     /**
